d41ca42b393627c1755ad1383d6db362fa2b599b
RrR2010 <richard.rsmi@hotmail.com>
2024-04-16 22:51:15 -0300

    Installed StoryBook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
+import { Metadata } from "next"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Resende Vip Tur",
   description: "Resende Vip Tur é uma companhia de translados, viagens e turismo que visa tornar sua viagem mais confortável, te levando para todos os lugares que você sonhar.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-BR">
       <body className={inter.className}>{children}</body>
